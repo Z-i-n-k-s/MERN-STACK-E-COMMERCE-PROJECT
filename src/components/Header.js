06be4c7c28d42fa18a2055419baa000f9643b1ce
Z-i-n-k-s <zishancodes1@gmail.com>
2024-05-21 22:55:08 +0600

    starting on navbar ui containing logo search-bar profile icon cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Logo from './Logo'
 const Header = () => {
   return (
     <header className='h-16 shadow-md'>
-      <div className='container mx-auto'>
+      <div className='h-full container mx-auto flex items-center'>
         <div className=''>
           <Logo w={90} h={50}/>
         </div>
