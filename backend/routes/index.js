15830da6cde6a33ac5e772d3c821b83796236ef0
SOMBIT MAZUMDER <115360765+SOMBIT4@users.noreply.github.com>
2024-06-19 02:51:58 +0600

    'alluserspaneldonewithdeletebutton'

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,10 +7,19 @@ const userSignInController = require("../controller/userSignIn")
 const userDetailsController = require('../controller/userDetails')
 const authToken = require('../middleware/authToken')
 const userLogout = require('../controller/userLogout')
+const allUsers = require('../controller/allUsers')
+const updateUser = require('../controller/updateUser')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
 router.get("/user-details",authToken,userDetailsController)
 router.get("/userLogout",userLogout)
+
+
+//admin panel
+router.get("/all-user",authToken,allUsers)
+router.post("/update-user",authToken,updateUser)
+
+
 
 module.exports = router
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,12 +8,13 @@ import Logo from "./Logo";
 import SummaryApi from "../common";
 import { toast } from 'react-toastify'
 import { setUserDetails } from '../store/userSlice'
+import ROLE from "../common/role";
 
 const Header = () => {
   const user = useSelector(state => state?.user?.user)
   const dispatch = useDispatch()
   const [menuDisplay,setMenuDisplay]=useState(false)
-  console.log("user header", user)
+  //console.log("user header", user)
   
 
 
@@ -52,24 +53,35 @@ const Header = () => {
         <div className="flex items-center gap-7">
             
             <div className="relative flex justify-center">
-            <div className="text-2xl cursor-pointer relative flex justify-center" onClick={()=>setMenuDisplay(preve => !preve )}>
-            {
-              user?.profilePic ? (
-                <img src={user?.profilePic} className="w-10 h-10 rounded-full" alt={user?.name} />
-              ) : (
-                <FaUserLarge />
-              )
-              
-            }
 
-          </div>
+              {
+                user?._id &&(
+                  <div className="text-2xl cursor-pointer relative flex justify-center" onClick={()=>setMenuDisplay(preve => !preve )}>
+                  {
+                    user?.profilePic ? (
+                      <img src={user?.profilePic} className="w-10 h-10 rounded-full" alt={user?.name} />
+                    ) : (
+                      <FaUserLarge />
+                    )
+                    
+                  }
+      
+                </div>
+                )
+              }
+            
 
 
               {
                 menuDisplay &&(
                   <div className="absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded">
                   <nav>
-                    <Link to={"admin-panel"} className="whitespace-nowrap hidden md:block hover:bg-slate-100 p-2" onClick={()=>setMenuDisplay(preve => !preve )}>Admin Panel</Link>
+                    {
+                      user?.role === ROLE.ADMIN &&(
+                        <Link to={"/admin-panel/all-products"} className="whitespace-nowrap hidden md:block hover:bg-slate-100 p-2" onClick={()=>setMenuDisplay(preve => !preve )}>Admin Panel</Link>
+                      )
+                    }
+                    
                   </nav>
               </div>
                 )
