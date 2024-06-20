8a6c2b38748b0012bd299b6782558bb2a35a48e2
Umme Ayesha <ummeayesha@Ummes-MacBook-Air.local>
2024-06-20 18:10:30 +0600

    buttondone

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -3,7 +3,11 @@ import React from 'react'
 const AllProducts = () => {
     return (
         <div>
-            all products
+            <div className='bg-white py-2 px-4 flex justify-between items-center' >
+                <h2 className='font-bold text-lg'>All Product</h2>
+                <button className='border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white  transition-all py-1 px-3 rounded-full'>Upload Product</button>
+            </div>
+
         </div>
     )
 }
