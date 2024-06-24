9016c27cca4324549aee3a109336a0c3990d586c
Z-i-n-k-s <zishancodes1@gmail.com>
2024-06-24 13:39:44 +0600

    frontend env

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,6 +1,6 @@
 import SignUP from "../pages/SignUP"
 
-const backendDomin = "http://localhost:8080"
+const backendDomin = process.env.REACT_APP_BACKEND_URL//"http://localhost:8080"
 
 
 const SummaryApi = {
