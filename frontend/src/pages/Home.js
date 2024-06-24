40e0ef668086fcf1042f97f98e8942ebbede166d
Z-i-n-k-s <zishancodes1@gmail.com>
2024-06-24 14:16:07 +0600

    instructions for search page were added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="h-screen flex flex-col justify-center items-center text-lg space-y-2">
+    <p className="flex justify-center">Instructions</p>
+    <p>To view search work</p>
+    <p>Go to All User section after LogIn</p>
+    <p>Please Login with</p>
+    <p>email - test1@gmail.com</p>
+    <p>password - test1</p>
+  </div>
   )
 }
 
