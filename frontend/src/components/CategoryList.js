f244e822886c8765ad38029ab212fa52872c41d8
Umme Ayesha <ummeayesha@Ummes-MacBook-Air.local>
2024-07-13 19:59:24 +0600

    Category Product Page

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import SummaryApi from '../common'
+import { Link } from 'react-router-dom'
 
 const CategoryList = () => {
   const [categoryProduct,setCategoryProduct] = useState([])
  const  [loading,setLoading] = useState(false)
-
+ const categoryLoading = new Array(13).fill(null)
 
   const fetchCategoryProduct = async() =>{
     setLoading(true)
@@ -22,20 +23,31 @@ const CategoryList = () => {
     <div className='container mx-auto  p-4 '>
       <div className='flex items-center gap-4 justify-between overflow-scroll scrollbar-none'>
       {
-        categoryProduct.map((product,index)=>{
-          return(
-            <div className='cursor-pointer'>
-              <div className='w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden p-3 bg-white flex items-center justify-center '>
-               <img src={product?.productImage[0]} alt={product?.category} className='h-full object-fill'/>
+        loading ? (
+  
+            categoryLoading.map((el,index)=>{
+              return(
+                <div className='h-16 w-16 md:w-20 md:h-20 rounded-full overflow-hidden bg-slate-200 animate-pulse' key={"categoryLoading"+index}>
+                  </div>
+              )
+            })   
+        ):
+        (
+          categoryProduct.map((product,index)=>{
+            return(
+              <Link to ={"/product-category/"+product?.category} className='cursor-pointer'key={product?.category}>
+                <div className='w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden p-4 bg-slate-200 flex items-center justify-center '>
+                 <img src={product?.productImage[0]} alt={product?.category} className='h-full object-scale-down mix-blend-multiply hover:scale-125 transition-all'/>
+                  
+                </div>
+                <p className='text-center text-sm md:text-base capitalize'>
+                  {product?.category}
+                </p>
+                </Link>
                 
-              </div>
-              <p className='text-center text-sm md:text-base capitalize'>
-                {product?.category}
-              </p>
-              </div>
-              
-          )
-        })
+            )
+          })
+        )
       }
       </div>
     </div>
