2ebcbbf21407962cb0df759fd9a0da35ca095317
Z-i-n-k-s <zishancodes1@gmail.com>
2024-07-15 17:55:36 +0600

    added limit to vercel load data

--- backend/controller/User/allUsers.js
+++ backend/controller/User/allUsers.js
@@ -1,27 +1,30 @@
-const userModel = require("../../models/userModel")
+const userModel = require("../../models/userModel");
 
-async function allUsers(req,res){
-    try{
+async function allUsers(req, res) {
+  try {
+    const { page = 1, limit = 10 } = req.query;  // Default values if not provided
 
-        
-        
+    const allUsers = await userModel.find()
+      .limit(Number(limit))
+      .skip((Number(page) - 1) * Number(limit));
 
-        const allUsers = await userModel.find()
+    const totalUsers = await userModel.countDocuments();
 
-        res.json({
-            message: "All user",
-            data:  allUsers,
-            success:true,
-            error:false
-
-        })
-    }catch(err){
-        res.status(400).json({
-            message : err.message || err,
-            error : true,
-            success : false,
-        })
-    }
+    res.json({
+      message: "All users",
+      data: allUsers,
+      totalPages: Math.ceil(totalUsers / limit),
+      currentPage: Number(page),
+      success: true,
+      error: false,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message || err,
+      error: true,
+      success: false,
+    });
+  }
 }
 
-module.exports  = allUsers
+module.exports = allUsers;
