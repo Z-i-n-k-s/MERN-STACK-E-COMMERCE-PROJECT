1128eb6887ecb9fad785c2740f7c9ab52f812479
SOMBIT MAZUMDER <115360765+SOMBIT4@users.noreply.github.com>
2024-07-17 07:47:53 +0600

    all products show in home done

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CategoryList from '../components/CategoryList'
 import BannerProduct from '../components/BannerProduct'
 import HorizontalCardProduct from '../components/HorizontalCardProduct'
+import VerticalCardProduct from '../components/VerticalCardProduct'
 
 const Home = () => {
   return (
@@ -9,6 +10,17 @@ const Home = () => {
       <CategoryList/>
     <BannerProduct/>
     <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"}/>
+    <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
+    <VerticalCardProduct   category={"mobiles"} heading={"Mobiles"}/>
+    <VerticalCardProduct   category={"mouse"} heading={"Mouses"}/>
+    <VerticalCardProduct   category={"televisions"} heading={"Televisions"}/>
+    <VerticalCardProduct   category={"refrigerator"} heading={"Refrigerator"}/>
+    <VerticalCardProduct   category={"printers"} heading={"Printers"}/>
+    <VerticalCardProduct   category={"speakers"} heading={"Speakers"}/>
+    <VerticalCardProduct   category={"camera"} heading={"Camera & Photography"}/>
+    <VerticalCardProduct   category={"trimmers"} heading={"Trimmers"}/>
+    <VerticalCardProduct   category={"processor"} heading={"Processor"}/>
+    <VerticalCardProduct   category={"earphones"} heading={"Earphones & Headphones"}/>
     </div>
   )
 }
